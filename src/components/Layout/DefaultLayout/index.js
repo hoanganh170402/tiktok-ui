e9ae952d5af2b3379a098bb4b72dccb7a939be61
unknown <hoanganh170402@gmail.com>
2025-05-14 14:26:10 +0700

    build default layout frame

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,12 +1,15 @@
-import Header from '~/components/Layout/components/Header';
+import classNames from 'classnames/bind';
+import styles from './Default.module.scss';
+
 import Sidebar from './Sidebar';
+
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
     return (
-        <div>
-            <Header />
-            <div className="container">
+        <div className={cx('wrapper')}>
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
