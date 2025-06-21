abf1e8208540a96a977958e4d9198e5321f2ceb6
unknown <hoanganh170402@gmail.com>
2025-06-21 14:08:10 +0700

    change contructor code #1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
 import { Home, Following, Upload, Profile, Search } from '~/pages';
-import configRoutes from '~/config/routes';
+import config from '~/config';
 
 // layout
-import { HeaderOnly } from '~/components/Layout';
+import { HeaderOnly } from '~/layouts';
 
 const publicRoutes = [
-    { path: configRoutes.home, component: Home },
-    { path: configRoutes.following, component: Following },
-    { path: configRoutes.upload, component: Upload, layout: HeaderOnly },
-    { path: configRoutes.profile, component: Profile },
-    { path: configRoutes.search, component: Search, layout: null },
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+    { path: config.routes.profile, component: Profile },
+    { path: config.routes.search, component: Search, layout: null },
 ];
 
 const privateRoutes = [];
