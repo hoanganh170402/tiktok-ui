5ee7189ad7bc271cc0dfbfa0168b7f1bf2f7a0cd
unknown <hoanganh170402@gmail.com>
2025-09-10 15:23:11 +0700

    console.log(process.env.REACT_APP_BASE_URL)

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -5,6 +5,7 @@ const request = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
 });
 
+console.log(process.env.REACT_APP_BASE_URL);
 const get = async (path, options = {}) => {
     const response = await request.get(path, options);
     return response.data;
